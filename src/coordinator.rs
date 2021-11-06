b09f0caf9a9b5e9595863179eb452eb4ac819a20
ShadowCurse <shadowcurse57@gmail.com>
2021-11-06 18:33:21 +0300

    added simple request-response functionality

--- src/coordinator.rs
+++ src/coordinator.rs
@@ -1,32 +1,127 @@
+use std::sync::{Arc, Mutex};
 use tonic::{transport::Server, Request, Response, Status};
 
 use coordinator::coordinator_server::{Coordinator, CoordinatorServer};
-use coordinator::{HelloReply, HelloRequest};
+use coordinator::{HelloReply, HelloRequest, JobDescription, WorkerDescription};
 
 pub mod coordinator {
     tonic::include_proto!("coordinator");
 }
 
+#[derive(Debug)]
+struct WorkerJob {
+    file: String,
+    worker: Option<String>,
+}
+
 #[derive(Debug, Default)]
-pub struct MyCoordinator {}
+pub struct CoordinatorData {
+    maps_jobs: Vec<WorkerJob>,
+    reduce_jobs: Vec<WorkerJob>,
+}
+
+#[derive(Debug, Default)]
+pub struct MyCoordinator {
+    data: Arc<Mutex<CoordinatorData>>,
+}
+
+impl MyCoordinator {
+    pub fn new(data: String) -> MyCoordinator {
+        let paths = std::fs::read_dir(&data).unwrap();
+        let maps_jobs = paths
+            .map(|path| WorkerJob {
+                file: path.unwrap().path().to_str().unwrap().to_string(),
+                worker: None,
+            })
+            .collect::<Vec<_>>();
+        for wj in maps_jobs.iter() {
+            println!("Found file: {}", wj.file);
+        }
+        let data = CoordinatorData {
+            maps_jobs,
+            reduce_jobs: Default::default(),
+        };
+        MyCoordinator {
+            data: Arc::new(Mutex::new(data)),
+        }
+    }
+
+    fn get_worker_name(&self, id: usize) -> Option<String> {
+        Some(
+            self.data
+                .lock()
+                .unwrap()
+                .maps_jobs
+                .get(id)?
+                .worker
+                .as_ref()?
+                .clone(),
+        )
+    }
+
+    fn get_and_assign_map_task(&self, worker: &str) -> Option<JobDescription> {
+        let mut data = self.data.lock().unwrap();
+        let job_pos = data.maps_jobs.iter().position(|mj| mj.worker.is_none())?;
+        data.maps_jobs[job_pos].worker = Some(worker.to_string());
+        Some(JobDescription {
+            id: job_pos as i32,
+            file_path: data.maps_jobs[job_pos].file.clone(),
+        })
+    }
+}
 
 #[tonic::async_trait]
 impl Coordinator for MyCoordinator {
-    async fn say_hello(&self, request: Request<HelloRequest>,) -> Result<Response<HelloReply>, Status> {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
         println!("Got a request: {:?}", request);
 
         let reply = coordinator::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into()
+            message: format!("Hello {}!", request.into_inner().name).into(),
         };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn request_job(
+        &self,
+        request: Request<WorkerDescription>,
+    ) -> Result<Response<JobDescription>, Status> {
+        let worker_name = request.into_inner().name;
+        println!("Got a request for a job from: {:?}", worker_name);
+
+        let reply = self
+            .get_and_assign_map_task(&worker_name)
+            .unwrap_or(Default::default());
+
+        Ok(Response::new(reply))
+    }
+
+    async fn job_done(
+        &self,
+        request: Request<JobDescription>,
+    ) -> Result<Response<JobDescription>, Status> {
+        let worker_name = self
+            .get_worker_name(request.into_inner().id as usize)
+            .unwrap_or(Default::default());
+        println!("Worker {} finished task", worker_name);
+
+        let reply = self
+            .get_and_assign_map_task(&worker_name)
+            .unwrap_or(Default::default());
 
         Ok(Response::new(reply))
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let coordinator = MyCoordinator::default();
+
+    let dir = "data".to_string();
+    let coordinator = MyCoordinator::new(dir);
 
     Server::builder()
         .add_service(CoordinatorServer::new(coordinator))
--- src/worker.rs
+++ src/worker.rs
@@ -1,5 +1,5 @@
 use coordinator::coordinator_client::CoordinatorClient;
-use coordinator::HelloRequest;
+use coordinator::WorkerDescription;
 
 pub mod coordinator {
     tonic::include_proto!("coordinator");
@@ -9,13 +9,22 @@ pub mod coordinator {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = CoordinatorClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
+    let request = tonic::Request::new(WorkerDescription {
+        name: "worker1".into(),
     });
 
-    let response = client.say_hello(request).await?;
+    let response = client.request_job(request).await?;
 
     println!("RESPONSE={:?}", response);
+
+    let mut r = response.into_inner();
+
+    while !r.file_path.is_empty() {
+        let request = tonic::Request::new(r);
+        let response = client.job_done(request).await?;
+        println!("RESPONSE={:?}", response);
+        r = response.into_inner();
+    }
 
     Ok(())
 }
